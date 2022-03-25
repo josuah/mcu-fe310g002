25c0af056d0a55bb2f2d1ef9c6c5b73db6dc890a
Josuah Demangeon <me@josuah.net>
2022-03-25 13:00:09 +0100

    s/sdk/mcu/

---
+++ functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#define MASK32(o)		(o == 31 ? 0xFFFFFFFF : (1u << (o + 1)) - 1)
+#define MASK(fld)		(MASK32(fld##_Msb) ^ MASK32(fld##_Lsb - 1))
+#define BIT(fld)		(1u << (fld))
+#define BITS(fld, val)		((val) << fld##_Lsb)
+#define FIELD(reg, fld)		(((reg) & MASK32(fld##_Msb)) >> fld##_Lsb)
+
+/* halt the execution */
+void __stop_program(void);
+
+#endif
--- init.c
+++ init.c
@@ -1,27 +1,21 @@
-#include <fe310g002.h>
+#include "libc.h"
+#include "registers.h"
+#include "functions.h"
 
 extern int main(void);
-extern void __reset_handler(void);
-
 extern char __data_start, __data_end, __data_load_start;
 extern char __bss_start, __bss_end, __stack_top;
 
 void
 __reset_handler(void)
 {
-	volatile char *src, *dst;
+	volatile char *dst, *src = &__data_load_start;
 
-	/* fill initialised and uninitialised variables */
-	src = &__data_load_start;
 	for (dst = &__data_start; dst < &__data_end; *dst++ = *src++);
 	for (dst = &__bss_start; dst < &__bss_end; *dst++ = 0);
-
 	main();
-	__stop_program();
-}
-
-void
-__stop_program(void)
-{
 	for (int volatile i = 0 ;; i++);
 }
+
+/* so that the debugger can immediately see which fault was triggered */
+void __null_handler(void)		{ for (int volatile i = 0;; i++); }
